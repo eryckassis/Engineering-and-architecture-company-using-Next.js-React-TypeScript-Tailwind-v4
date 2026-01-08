ca02d79c730874b499e2b5e009028212bbfaa869
Eryck Assis <eng.assis.dev@gmail.com>
2026-01-08 14:47:08 -0300

    feat: Implement CTAWindow component with props for image, header, callout, and description

--- src/components/sections/CTAWindow.tsx
+++ src/components/sections/CTAWindow.tsx
@@ -1,2 +1,24 @@
 import type { SectionProps } from "@/types";
 import { cn } from "@/lib/utils";
+
+import Copy from "@/components/ui/Copy";
+
+interface CTAWindowProps extends SectionProps {
+    img: string;
+    header: string;
+    callout: string;
+    description: string;
+}
+
+export default function CTAWindow({
+    img,
+    header,
+    callout,
+    description,
+    className,
+    id,
+}: CTAWindowProps) {
+    return (
+        
+    )
+}
